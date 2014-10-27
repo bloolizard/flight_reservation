ff6e5d6938d3efbf2589511ab21639fe479d41b4
Edwin Villanueva <edwincv0@gmail.com>
2014-10-27 03:42:31 -0700

    added fn to send obj to backend, added table

--- public/js/flt_reserve_src.js
+++ public/js/flt_reserve_src.js
@@ -3,37 +3,98 @@ function getFormData(form){
             departcity: form.departcity.value,
             departdate: form.departdate.value,
             arrivalcity: form.arrivalcity.value,
-            returndate: form.returndate.value,
+            returndate: form.returndate.value
             };
 }
 
 // makes an ajax call to backend
-function makeAnAjaxCall(){
+function makeAnAjaxCall(form, callback){
+    var data = getFormData(form);
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/reservation');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.addEventListener('readystatechange', function(){
+       if (xhr.status === 200 && xhr.readyState === 4){
+           callback(xhr.responseText);
+       }
+    });
+    xhr.send(JSON.stringify(data));
+
+}
+
+// makes an ajax call to backend
+function getReservation(callback){
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/getreservations');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.addEventListener('readystatechange', function(){
+        if (xhr.status === 200 && xhr.readyState === 4){
+            callback(xhr.responseText);
+        }
+    });
+    xhr.send();
 
 }
 
 window.addEventListener('load', function(){
+    function populateDataTable(data_obj){
+        var table = document.getElementById('my_flt_table');
+        var tr = document.createElement('tr');
 
+        // make an element for every prop in obj
+        for (var i in data_obj){
+            var td = document.createElement('td');
+            td.innerHTML = data_obj[i];
+            tr.appendChild(td);
+
+        }
+
+
+        table.appendChild(tr);
+        console.log(data_obj.departcity);
+        console.log(data_obj.departdate);
+        console.log(data_obj.arrivalcity);
+        console.log(data_obj.returndate);
+    }
 
     // grab main window
     var main = document.getElementById('main');
     console.log('main');
 
+    // submit btn --> sends form data to server
+    var submit_btn = document.getElementById('submit');
+    submit_btn.addEventListener('click', function(){
+        var flt_reserve_frm = document.forms.flt_reserve_form;
+        makeAnAjaxCall(flt_reserve_frm, function(){
+            alert('Thank you for your reservation');
+        });
+    });
+
+
+    // retrieves reservation object from server
     var flt_act_btn = document.getElementById('my_flight_act_btn');
     flt_act_btn.addEventListener('click', function(){
-        console.log('Hey I was clicked');
         // clear the main section
-
+        var flt_reserve_section = document.getElementById('flt_reserve_section');
+        flt_reserve_section.style.display = 'none';
         // perform an AJAX call
-
+        getReservation(function(data){
+           var _data = JSON.parse(data);
+           for (var i in _data){
+                console.log(_data[i]);
+               // populate page
+               populateDataTable(_data[i]);
+            }
+        });
         // display the AJAX on the page
+        var my_flt_section = document.getElementById('my_flt_section');
+        my_flt_section.style.display = 'block';
         // in a new element way.
 
 
-        main.innerHTML = "I got some new data for you.  And I can show you your flights.";
 
-        var return_btn = document.createElement('button');
-        return_btn.innerHTML = "Go Back"
-        main.appendChild(return_btn);
+//        var return_btn = document.createElement('button');
+//        return_btn.innerHTML = "Go Back"
+//        main.appendChild(return_btn);
     });
 });
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var candidates = [];
+var reservations = [];
 
 /* GET home page. */
 router.get('/json', function(req, res) {
@@ -26,5 +27,20 @@ router.get('/reservation', function(req, res) {
     res.render('flight_reserve');
 });
 
+//todo: store reservation post obj in array
+router.post('/reservation', function(req, res) {
+    console.log('Reservation accepted');
+
+    reservations.push(req.body);
+    console.log(reservations);
+    // send back the candidates
+    res.status(200).json({message: 'Thanks for the info'});
+});
+
+//get reservations
+router.post('/getreservations', function(req, res) {
+    // send back the candidates
+    res.status(200).json(reservations);
+});
 
 module.exports = router;
